c8ffff3713a27f9438eec3952ea2a73d9da80142
nafi-ullah <shafinnafiullah@gmail.com>
2024-06-25 19:38:34 +0600

    hall and time added

--- app/Invitation.js
+++ app/Invitation.js
@@ -8,7 +8,7 @@ const Invitation = ({ guestName }) => {
       <div className='text-5xl w-full flex justify-center my-7 mb-14 pt_sarief'>Wedding Invitation</div>
       <div className='parisienne w-full text-5xl flex justify-center '>Muid Hasan Mahin</div>
       <div className='parisienne w-full text-8xl flex justify-center'>&</div>
-      <div className='parisienne w-full text-5xl flex justify-center'>Afroza Jabin Ela</div>
+      <div className='parisienne w-full text-5xl flex justify-center'>Afroze Jabin Ela</div>
       <div className='w-full my-6 flex justify-center pt_sarief text-2xl mt-20'>Save The Date </div>
       <div className='w-full flex justify-center '>
         <div className='w-1/2 h-1 bg-[#E1B954] '></div>
@@ -16,8 +16,11 @@ const Invitation = ({ guestName }) => {
       <div className='w-full my-3 flex justify-center pt_sarief text-4xl'>28th June 2024 </div>
       <div className='w-full flex justify-center '>
         <div className='w-1/2 h-1 bg-[#E1B954] '></div>
+       
       </div>
-      <div className='w-full text-center px-5 pt-5 pb-10 flex justify-center pt_sarief text-2xl'>46/6, Haque Tower, M-block, Main Road, Banasree, Dhaka-1219 Dhaka </div>
+      <div className='w-full my-6 flex justify-center pt_sarief text-2xl mt-2'>02:00 PM Friday </div>
+      <div className='w-full mt-6 flex justify-center pt_sarief text-2xl '>HAQUE's Banquet Hall & Restaurant   </div>
+      <div className='w-full text-center px-5  pb-10 flex justify-center pt_sarief text-lg'>46/6, Haque Tower, M-block, Main Road, Banasree, Dhaka-1219  </div>
     </div>
   );
 }
