64fd89bcc2885d7d510bf79ef13996f11a0a62cf
nafi-ullah <shafinnafiullah@gmail.com>
2024-06-25 19:24:46 +0600

    both side padding fixed

--- app/InviatationCard.js
+++ app/InviatationCard.js
@@ -3,7 +3,8 @@ import Invitation from './Invitation';
 
 const InviatationCard = ({ guestName }) => {
   return (
-    <div id="invitation-card" style={{ width: '1850px', height: '800px' }}>
+    <div id="invitation-card" style={{ width: '1850px', height: '900px' }}>
+          <div className="w-full  h-[65px] bg-black"></div>
       <div className="w-full h-2 bg-yellow-800"></div>
       <div className="w-full h-[770px] flex justify-center bg-black">
         <div className="md:w-1/5 w-1/6"><img src="sidebar1.png" alt="side image" /></div>
@@ -13,7 +14,7 @@ const InviatationCard = ({ guestName }) => {
         <div className=" md:w-1/5 w-1/6"><img  src="sidebar2.png" alt="side image" /></div>
       </div>
       <div className="w-full h-2 bg-yellow-800"></div>
-      <div className="w-full  h-48 bg-black"></div>
+      <div className="w-full  h-[65px] bg-black"></div>
     </div>
   );
 }
