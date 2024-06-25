052b7051becd5c3bacff30df89bded7205603fbe
nafi-ullah <shafinnafiullah@gmail.com>
2024-06-25 19:50:13 +0600

    Added father name mother name

--- app/Invitation.js
+++ app/Invitation.js
@@ -7,9 +7,11 @@ const Invitation = ({ guestName }) => {
       <div className='w-full flex justify-center text-2xl pt_sarief'>You are requested to attend the wedding ceremony of </div>
       <div className='text-5xl w-full flex justify-center my-7 mb-14 pt_sarief'>Wedding Invitation</div>
       <div className='parisienne w-full text-5xl flex justify-center '>Muid Hasan Mahin</div>
-      <div className='parisienne w-full text-8xl flex justify-center'>&</div>
+      <div className='pt_sarief w-full text-xl flex justify-center italic'>Eldest son of S.M.Sayed and Nargis Parvin Nazma</div>
+      <div className='parisienne w-full text-6xl flex justify-center'>&</div>
       <div className='parisienne w-full text-5xl flex justify-center'>Afroze Jabin Ela</div>
-      <div className='w-full my-6 flex justify-center pt_sarief text-2xl mt-14'>Save The Date </div>
+      <div className='pt_sarief w-full text-xl mt-2 flex justify-center italic'>Youngest daughter of Azizul Haque and Helena akter</div>
+      <div className='w-full my-4 flex justify-center pt_sarief text-2xl mt-10'>Save The Date </div>
       <div className='w-full flex justify-center '>
         <div className='w-1/2 h-1 bg-[#E1B954] '></div>
       </div>
